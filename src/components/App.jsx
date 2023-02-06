873ac5e19d7912f80c76590c97dd1d93a49f4bd6
Petro Psekho <psekho.pp@gmail.com>
2023-02-06 22:18:41 +0200

    task profile in progress

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { Profile } from 'components/Profile/Profile';
+import user from '../user';
+
+export const App = () => {
+  return <div><Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/></div>;
+};
