923beef8fcc4c9539566efd645fe48ef439e08f8
Petro Psekho <psekho.pp@gmail.com>
2023-02-07 15:33:22 +0200

    add render Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
 import user from '../user';
+import data from '../data';
 
 export const App = () => {
-  return <div><Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/></div>;
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
+    </div>
+  );
 };
