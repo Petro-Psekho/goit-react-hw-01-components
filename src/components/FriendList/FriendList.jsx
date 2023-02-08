8dc36e8e39232b67308efc2f940e8294038bcdae
Petro Psekho <psekho.pp@gmail.com>
2023-02-08 09:26:47 +0200

    add propTypes to FriendList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,24 +1,33 @@
 // import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import css from 'components/FriendList/FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul classNane={css.friendList} >
+    <ul classNane={css.friendList}>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id} classNane={css.item}>
+          <span classNane={css.status}></span>
+          <img
+            classNane={css.avatar}
+            src={avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p classNane={css.name}>{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-      {friends.map(friend => (
-        <li key ={friend.id} classNane={css.item}>
-  <span classNane={css.status}></span>
-  <img classNane={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
-          <p classNane={css.name}>{friend.name}</p>
-</li>))}
-
-</ul>
-  )
-}
-
-  // {
-  //   "avatar": "https://cdn-icons-png.flaticon.com/512/1998/1998592.png",
-  //   "name": "Mango",
-  //   "isOnline": true,
-  //   "id": 1812
-  // },
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
